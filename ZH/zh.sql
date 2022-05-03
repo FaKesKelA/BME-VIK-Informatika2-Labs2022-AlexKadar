0ea88b4c0c47fc10b9339a8ec4d52a876ac99124
FaKesKelA <36206088+FaKesKelA@users.noreply.github.com>
2022-05-03 10:58:18 +0200

    Add files via upload

---
+++ ZH/zh.sql
@@ -0,0 +1,19 @@
+﻿DROP DATABASE IF EXISTS zh ;
+
+CREATE DATABASE zh
+	DEFAULT CHARACTER SET utf8
+	DEFAULT COLLATE utf8_general_ci;
+
+use zh;
+
+CREATE TABLE terem(
+	id int not null auto_increment primary key,
+	epulet varchar(50) not null,
+	terem_nev varchar(50) not null,
+	ferohely int not null
+);
+
+INSERT INTO terem (epulet, terem_nev, ferohely) VALUES ('I', 'IE007', 200);
+INSERT INTO terem (epulet, terem_nev, ferohely) VALUES ('I', 'IB028', 400);
+INSERT INTO terem (epulet, terem_nev, ferohely) VALUES ('St', 'StNagy', 380);
+INSERT INTO terem (epulet, terem_nev, ferohely) VALUES ('Ch', 'ChMax', 550);
